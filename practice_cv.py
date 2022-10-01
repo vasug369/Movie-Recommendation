adf0c4bf68c7665a2c246a3a201656593a773530
vasu <vasupankajdbd@gmail.com>
2022-10-02 00:28:47 +0530

    commit

--- practice_cv.py
+++ practice_cv.py
@@ -3,7 +3,7 @@ import numpy as np
 vid=cv.VideoCapture(0)
 while(True):
 	isTrue,frame=vid.read()
-	np.zeros(frame,dtype=uint8)
+	blank=np.zeros(frame,dtype=uint8)
 	cv.imshow('myvideo',frame)
 	if(cv.waitKey(1) & 0xFF==ord('q')):
 		break
